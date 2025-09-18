1251ba8847dbcf455e9a0dba8a7b9539d54141fc
rendra-pramudya <rendra.pramudya@gmail.com>
2025-09-18 06:08:42 +0300

    Add map drawing, icon placement, and animated travel to memorized locations

---
+++ map-icons.js
@@ -0,0 +1,23 @@
+// Tank icon SVG
+export const tankIcon = L.icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/3079/3079326.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+});
+
+// Car icon SVG
+export const carIcon = L.icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/743/743007.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+});
+
+// House icon SVG
+export const houseIcon = L.icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/616/616494.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+});
